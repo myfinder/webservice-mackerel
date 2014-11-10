849ebade43854031a70e65d9a868d6bf52a71ee5
Tatsuro Hisamori <medianetworks@gmail.com>
2014-11-10 12:55:42 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+Mackerel::Client - API Client for mackerel.io
+
+# SYNOPSIS
+
+    use Mackerel::Client;
+
+# DESCRIPTION
+
+Mackerel::Client is API Client for mackerel.io
+
+# LICENSE
+
+Copyright (C) Tatsuro Hisamori.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+Tatsuro Hisamori <myfinder@cpan.org>
---
+++ lib/Mackerel/Client.pm
@@ -0,0 +1,39 @@
+package Mackerel::Client;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Mackerel::Client - It's new $module
+
+=head1 SYNOPSIS
+
+    use Mackerel::Client;
+
+=head1 DESCRIPTION
+
+Mackerel::Client is ...
+
+=head1 LICENSE
+
+Copyright (C) Tatsuro Hisamori.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+Tatsuro Hisamori E<lt>medianetworks@gmail.comE<gt>
+
+=cut
+
