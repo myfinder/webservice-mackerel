2d463db3cd23173df622790b15c13f3f867ecf72
yoheimuta <yoheimuta@gmail.com>
2016-11-20 12:15:21 +0900

    Added a new method 'get_host_metrics'

--- lib/WebService/Mackerel.pm
+++ lib/WebService/Mackerel.pm
@@ -111,6 +111,20 @@ sub post_host_metrics {
     return $res->{content};
 }
 
+sub get_host_metrics {
+    my ($self, $host_id, $query_parameters) = @_;
+    my $uri = URI->new($self->{mackerel_origin});
+    $uri->path("/api/v0/hosts/$host_id/metrics");
+    $uri->query_form($query_parameters);
+    my $res = $self->{agent}->request('GET', $uri->as_string, {
+            headers => {
+                'content-type' => 'application/json',
+                'X-Api-Key'    => $self->{api_key},
+            },
+        });
+    return $res->{content};
+}
+
 sub get_latest_host_metrics {
     my ($self, $args) = @_;
     my $path = '/api/v0/tsdb/latest?hostId=' . $args->{hostId} . '&name=' . $args->{name};
